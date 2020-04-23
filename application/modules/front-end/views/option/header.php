922006701d9e2bdcdd0d6294c3fdc9aa84d057ab
worapongsrisawan <famnoii2550@gmail.com>
2020-04-23 13:46:35 +0700

    fix

--- application/modules/front-end/views/option/header.php
+++ application/modules/front-end/views/option/header.php
@@ -5,7 +5,7 @@
 
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="author" content="SemiColonWeb" />
-
+	<link rel="shortcut icon" href="public/assets/front-end/images/logo_ban_new.png" />
 	<!-- Stylesheets
 	============================================= -->
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i" rel="stylesheet" type="text/css" />
